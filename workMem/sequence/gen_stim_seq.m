43d385dfabc9e79dd42067d88e564053ffab1db5
gaozhy <zhiyao.gao@gmail.com>
2019-02-21 16:47:44 +0000

    final version-- check 1

--- workMem/sequence/gen_stim_seq.m
+++ workMem/sequence/gen_stim_seq.m
@@ -8,8 +8,8 @@ for r = 1:4
     Targ2=target_id(:,2);
     
     seq_data=table(Level,Correc_id,Targ1,Targ2);
-    filename=sprintf('stim_run%d.csv',r);
-    filename2=sprintf('run%d.csv',r);
+    filename=sprintf('stim_run%d.csv',r-1);
+    filename2=sprintf('run%d.csv',r-1);
     csvwrite(filename2,trial_sample);
     writetable(seq_data,filename);
 end
